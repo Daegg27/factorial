3eca552f02269242a776e6a161cb8d79385b4870
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-24 16:52:03 -0400

    factorial

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,16 @@
-exports.factorial = function(num) {
+// exports.factorial = function(num)
+
+function factorial(num) {
+  
+
+if (num === 0 || num === 1) {
+        return 1
+        
+    }
+
+    let answer = num * factorial(num - 1)
+    return answer
+
 };
+
+console.log(factorial(45))
